192af1258471edc6f497c4cf9bf9cde6544edb0e
Michael <michael@managedkaos.com>
2019-04-14 22:43:53 -0700

    Add case statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,20 +20,85 @@ git add ${GITHUB_WORKSPACE}/${GITHUB_EVENT_NAME}.json
 
 # switch on the event name to finalize the action
 case $GITHUB_EVENT_NAME in
+    check_run)
+        push_local_changes;
+        ;;
+    check_suite)
+        push_local_changes;
+        ;;
+    commit_comment)
+        push_local_changes;
+        ;;
+    create)
+        push_local_changes;
+        ;;
+    delete)
+        push_local_changes;
+        ;;
+    deployment)
+        push_local_changes;
+        ;;
+    deployment_status)
+        push_local_changes;
+        ;;
     fork)
         push_local_changes;
         ;;
-
+    gollum)
+        push_local_changes;
+        ;;
+    issue_comment)
+        push_local_changes;
+        ;;
+    issues)
+        push_local_changes;
+        ;;
+    label)
+        push_local_changes;
+        ;;
+    member)
+        push_local_changes;
+        ;;
+    milestone)
+        push_local_changes;
+        ;;
+    page_build)
+        push_local_changes;
+        ;;
+    project)
+        push_local_changes;
+        ;;
     project_card)
         push_local_changes;
         ;;
-
-    push)
+    project_column)
         push_local_changes;
         ;;
-
+    public)
+        push_local_changes;
+        ;;
     pull_request)
         exit 78
         ;;
+    pull_request_review_comment)
+        exit 78
+        ;;
+    pull_request_review)
+        exit 78
+        ;;
+    push)
+        push_local_changes;
+        ;;
+    repository_dispatch)
+        push_local_changes;
+        ;;
+    release)
+        push_local_changes;
+        ;;
+    status)
+        push_local_changes;
+        ;;
+    watch)
+        push_local_changes;
+        ;;
 esac
-
