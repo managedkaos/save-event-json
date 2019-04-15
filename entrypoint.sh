ac92935da07c46088b31d0b8ef08d6cd6d06dfd4
Michael <michael@managedkaos.com>
2019-04-14 19:59:19 -0700

    Update path to saved JSON; add git commands to commit the file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -e
-jq -r . < $GITHUB_EVENT_PATH > $GITHUB_WORKSPACE/event.json
+jq -r . < ${GITHUB_EVENT_PATH} > ${GITHUB_WORKSPACE}/${GITHUB_EVENT_NAME}.json
+
+git config --global user.email "noreply@github.com"
+git config --global user.name "GitHub Actions Bot"
+git add ${GITHUB_WORKSPACE}/${GITHUB_EVENT_NAME}.json
+git commit -m "${GITHUB_EVENT_NAME} JSON saved by ${GITHUB_ACTION}"
+git push -u origin HEAD
